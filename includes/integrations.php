390d85071dec31b8b3ee89b41dfc6b2c77ec1763
franpass87 <francesco.passeri@gmail.com>
2025-09-11 13:10:35 +0200

    feat: initialize consent tracking

--- includes/integrations.php
+++ includes/integrations.php
@@ -24,11 +24,11 @@ function rbf_print_gtag($ga4_id, $send_page_view = true) {
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr($ga4_id); ?>"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({event:'consent', 'analytics_storage':'denied'});
         function gtag(){ dataLayer.push(arguments); }
         gtag('js', new Date());
         gtag('config', '<?php echo esc_js($ga4_id); ?>', { 'send_page_view': <?php echo $send_page_view ? 'true' : 'false'; ?> });
     </script>
-    <?php echo "<script>window.dataLayer.push({event:'consent', 'analytics_storage':'denied'});</script>"; ?>
     <?php
 }
 
@@ -43,10 +43,12 @@ function rbf_print_gtm($gtm_id, $init_datalayer = true) {
     $printed = true;
     if ($init_datalayer) {
         ?>
-        <script>window.dataLayer = window.dataLayer || [];</script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({event:'consent', 'analytics_storage':'denied'});
+        </script>
         <?php
-        echo "<script>window.dataLayer.push({event:'consent', 'analytics_storage':'denied'});</script>";
-        }
+    }
     ?>
     <script>
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?"&l="+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','<?php echo esc_js($gtm_id); ?>');
@@ -57,11 +59,11 @@ function rbf_print_gtm($gtm_id, $init_datalayer = true) {
 /**
  * Update analytics consent state in dataLayer.
  *
- * @param bool $analytics_allowed Whether analytics storage is granted.
+ * @param bool $granted Whether analytics storage is granted.
  */
-function rbf_update_consent($analytics_allowed) {
-    $analytics_status = $analytics_allowed ? 'granted' : 'denied';
-    echo "<script>window.dataLayer.push({event:'consent', 'analytics_storage':'{$analytics_status}'});</script>";
+function rbf_update_consent($granted) {
+    $status = $granted ? 'granted' : 'denied';
+    echo "<script>window.dataLayer.push({event:'consent', 'analytics_storage':'{$status}'});</script>";
 }
 add_action('rbf_update_consent', 'rbf_update_consent');
 
